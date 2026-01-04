f8c393307bdd3cd331f5afc4580a5fbfa4e8850e
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-04 22:27:30 +0545

    feat: Add datatypes.java to demonstrate Java data types and variables

---
+++ src/datatypes.java
@@ -0,0 +1,20 @@
+public class datatypes {
+    //in this program we will learn about the different datatypes and variables in java
+    //variables are used to store data like x=5
+    //there are basically 2 data types
+    //1. primitive data types uses stack
+    //2. non-primitive data types uses heap memory
+    //primitive is like storing data directly like int float char boolean
+    //non primitive is like storing memory address of data like arrays strings objects that points to heap
+
+    public static void main(String [] args){
+        int age = 25; //this is integer data type
+        System.out.printf("You are %d years old\n", age);
+
+        String name = "Kiran";
+        System.out.printf("Hi Mr %s", name);
+
+
+    }
+
+}
