eb3761ca8f50734b9804c234b994ea829e829766
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-04 21:31:40 +0545

    Update .gitignore and modify Main.java for initial program output

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-//simple java program to print Hello, World!
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, Github!");
+    //this is my first java program using IntelliJ IDEA
+    public static  void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("This is my first java program in intelliJ IDEA");
+        System.out.println("I am learning the New Programming Concepts");
     }
-
 }
