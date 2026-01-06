436e73685176c76f47c5adcf87e596ba669b2144
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-06 22:26:19 +0545

    feat: Enhance ifStatement.java to include age classification logic

--- src/ifStatement.java
+++ src/ifStatement.java
@@ -6,6 +6,7 @@ public class ifStatement {
         //lets see an example
         Scanner scanner = new Scanner(System.in);
         int x;
+        int age;
         System.out.println("Enter a number to be checked");
         x= scanner.nextInt();
 
@@ -15,6 +16,24 @@ public class ifStatement {
         }
         else {
             System.out.println("The number is odd");
+        }
+
+        //if else-if and else
+        System.out.println("Enter your age");
+        age= scanner.nextInt();
+
+        if (age>=60)
+        {
+            System.out.println("You are senior");
+        } else if (age>=18) {
+            System.out.println("You are an adult");
+
+        } else if (age==0) {
+            System.out.println("You are baby");
+
+        }
+        else {
+            System.out.println("Invalid age");
         }
 
 
