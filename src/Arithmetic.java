760d06b4255f6a33c09f2fb944616be0b67ba921
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-06 21:15:47 +0545

    feat: Add Arithmetic.java for demonstrating basic arithmetic operations

---
+++ src/Arithmetic.java
@@ -0,0 +1,29 @@
+public class Arithmetic {
+    //in this section we will learn about the different arithmetic operations
+
+    public static void main(String[] args){
+        int x =10;
+        int y= 3;
+        int z;
+
+        //Addition
+        z = x+y;
+        System.out.println("The addition of x and y is: "+z);
+
+        //subtraction
+        z=x-y;
+        System.out.println("The subtraction of x and y is: "+z);
+
+        //multiplication
+        z=x*y;
+        System.out.println("Product of x and y is: "+z);
+
+        //division
+        z=x/y;
+        System.out.println("Division of x and y is:"+z);
+
+        //remainder
+        z=x%y;
+        System.out.println("Remainder of x /y is: "+z);
+    }
+}
