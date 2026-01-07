9f101c907e8b457c5aac58972092cf17b458ab01
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-07 21:37:27 +0545

    feat: Add Animal and Dog classes with eating behavior in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 public class Main {
+     static class Animal{
+       void Eat(){
+           System.out.println("animal is eating");
+       }
+
+    }
+
+     static  class Dog extends  Animal {
+        @Override
+        void Eat() {
+            super.Eat();
+        }
+    }
+
     //this is my first java program using IntelliJ IDEA
-    public static  void main(String[] args){
+    public   void main(String[] args){
+        Dog dog1 = new Dog();
+        dog1.Eat();
         System.out.println("Hello World");
         System.out.println("This is my first java program in intelliJ IDEA");
         System.out.println("I am learning the New Programming Concepts");
+
     }
 }
