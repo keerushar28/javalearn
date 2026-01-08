c5c92f24d5161365e87dfa10c34bf673c7cd5c32
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-08 09:02:28 +0545

    feat: Enhance mathJava class with additional mathematical operations

--- src/mathJava.java
+++ src/mathJava.java
@@ -4,6 +4,27 @@ public class mathJava {
         System.out.println(Math.PI);
         System.out.println(Math.E);
 
+        //some other concept related to math class in java
+        double result;
+        result = Math.abs(-5);
+        System.out.println(result);
+        result = Math.max(10,20);
+        System.out.println(result);
+        result = Math.min(10,20);
+        System.out.println(result);
+        result = Math.pow(2,3); //base,power
+        System.out.println(result);
+        result = Math.sqrt(81);
+        System.out.println(result);
+        result = Math.round(5.25);
+        System.out.println(result);
+        result = Math.ceil(3.365);
+        System.out.println(result);
+        result = Math.floor(3.365);
+        System.out.println(result);
+
+
+
 
     }
 }
