0496318e7379609d7725c396cf43fb4bcceb2099
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-09 21:41:34 +0545

    feat: Add stringMethods class with length method demonstration

---
+++ src/stringMethods.java
@@ -0,0 +1,8 @@
+public class stringMethods {
+    public static void main(String[] args){
+        //length method
+        String name  = "Kiran Gautam";
+        int len = name.length();
+        System.out.println(len);
+    }
+}
