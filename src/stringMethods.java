d991fb8284923cc1523f3dbfd4f62b4dacb6f154
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-09 21:54:23 +0545

    feat: Enhance stringMethods class with indexOf and lastIndexOf method demonstrations

--- src/stringMethods.java
+++ src/stringMethods.java
@@ -5,8 +5,18 @@ public class stringMethods {
         int len = name.length();
         System.out.println(len);
 
-        //charAt
+        //charAt is used to find the character at given index
         char letter = name.charAt(10);
         System.out.println(letter);
+
+        //indexOf is used to find the index of the given char
+        int index = name.indexOf('a');
+        System.out.println(index);
+        //lastIndex gives last index for repeated
+        int lastIndex = name.lastIndexOf('a');
+        System.out.println(lastIndex);
+
+
+
     }
 }
