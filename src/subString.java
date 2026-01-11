9c60ff208964a92e367b8cc88ad1c36ffbfc9a88
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-11 20:25:40 +0545

    feat: Enhance subString class to accept user input for dynamic username and domain extraction

--- src/subString.java
+++ src/subString.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 public class subString {
     public static void main(String[] args)
     {
+        /*
         String email = "gautamkiran878@gmail.com";
         String username = email.substring(0,14); //0 is start index and 14 is last index but not
         // included
@@ -15,6 +17,33 @@ public class subString {
         System.out.println(username1);
         String domain1 = email1.substring(email1.indexOf('@')+1);
         System.out.println(domain1);
+        */
+         //lets work with the scanner
+        Scanner scanner = new Scanner(System.in);
+        String email;
+        String username;
+        String domain;
+
+        System.out.print("Enter your email: ");
+        email = scanner.nextLine();
+
+        try {
+            if (!email.contains("@"))
+            {
+                System.out.println("Please enter the valid email");
+            }
+            username = email.substring(0,email.indexOf('@'));
+            System.out.println("Your username based on your email is "+username);
+            domain = email.substring(email.indexOf('@')+1);
+            System.out.println("You are using: "+domain);
+
+        } catch (Exception e) {
+            System.out.println("Something went wrong "+e);
+        }
+
+
+
+        scanner.close();
 
 
     }
