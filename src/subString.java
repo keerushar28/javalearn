a0145ba91a542d46c7dfbc8a3b3e5d559cb483e4
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-11 20:13:35 +0545

    feat: Update subString class to demonstrate dynamic username and domain extraction from email

--- src/subString.java
+++ src/subString.java
@@ -7,5 +7,15 @@ public class subString {
         System.out.println(username);
         String domain = email.substring(15);
         System.out.println(domain);
+        //this could be useful for auto generating the username from the email
+        //but this is manually assigning the number is not effective because email can be of
+        // different length so we use indexOf method,
+        String email1 = "bro@gmail.com";
+        String username1 = email1.substring(0,email1.indexOf('@'));
+        System.out.println(username1);
+        String domain1 = email1.substring(email1.indexOf('@')+1);
+        System.out.println(domain1);
+
+
     }
 }
