45e6146ac89d39f05690fa21e3bb8909e8987a3b
Kiran Gautam <gautamkiran878@gmail.com>
2026-01-11 21:15:35 +0545

    feat: Add ternary operator example in new ternary class

---
+++ src/ternary.java
@@ -0,0 +1,11 @@
+public class ternary {
+    public static void main(String[] args)
+    {
+        //ternary operator (condition)?true:false
+        int score = 8;
+        String res = (score>=80)?"PASS":"FALSE";
+        System.out.println(res);
+    }
+
+}
+
